0cbf93405f0534a646beada7e55397c05888e2c2
Rafael Lemes <rafael.lemes@avmb.com.br>
2024-02-01 16:05:37 -0300

    Criação componente Banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="main-container">
-    <h1>Home</h1>
-  </div>
+	<Banner />
+	<div class="main-container">
+		<h1>Monte o seu burguer:</h1>
+		formulario
+	</div>
 </template>
 
 <script>
+import Banner from '@/components/Banner.vue';
+
+export default {
+	name: "Home",
+	components: {
+		Banner
+	}
+}
 
 </script>
