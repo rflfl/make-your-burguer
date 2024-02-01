282d7ffea86f872a0325e17980b4bd1bb35568e1
Rafael Lemes <rafael.lemes@avmb.com.br>
2024-02-01 17:10:38 -0300

    criado formulario de montagem do burger

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer id="footer">
-	<p>Make Your Burguer &copy; 2024</p>
+	<p>Make Your Burger &copy; 2024</p>
   </footer>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,20 @@
 <template>
 	<Banner />
 	<div class="main-container">
-		<h1>Monte o seu burguer:</h1>
-		formulario
+		<h1>Monte o seu burger:</h1>
+		<BurgerForm />
 	</div>
 </template>
 
 <script>
 import Banner from '@/components/Banner.vue';
+import BurgerForm from '@/components/BurgerForm.vue';
 
 export default {
 	name: "Home",
 	components: {
-		Banner
+		Banner,
+		BurgerForm
 	}
 }
-
 </script>
