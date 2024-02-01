f47e9448506b31a0975d589bb7bb0410f8bc6a8c
Rafael Lemes <rafael.lemes@avmb.com.br>
2024-02-01 14:58:25 -0300

    Estrutura inicial

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+	<nav>
+		<router-link to="/">
+			<img src="" id="logo" alt="Logo">
+		</router-link>
+		<router-link to="/">Home</router-link> |
+		<router-link to="/pedidos">Pedidos</router-link>
+	</nav>
+</template>
+<script>
+export default {
+	name: "Navbar"
+}
+</script>
