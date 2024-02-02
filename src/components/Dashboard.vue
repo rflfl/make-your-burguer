d9cc26b2a7c9298b7975d40f285bfd4c79c8842d
Rafael Lemes <rafael.lemes@avmb.com.br>
2024-02-02 15:30:58 -0300

    listando os status dos pedidos

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,98 +1,129 @@
 <template>
-  <div id="burger-table">
-	<div>
-		<div id="burger-table-heading">
-			<div class="order-id">#</div>
-			<div>Cliente:</div>
-			<div>Pão:</div>
-			<div>Carne:</div>
-			<div>Opcionais:</div>
-			<div>Ações:</div>
-		</div>
-		<div id="burger-table-rows">
-			<div class="burger-table-row">
-				<div class="order-number">1</div>
-				<div>João</div>
-				<div>Pão de trigo</div>
-				<div>Maminha</div>
-				<div>
-					<ul>
-						<li>Salame</li>
-						<li>Tomate</li>
-					</ul>
-				</div>
-				<div>
-					<select name="status" class="status">
-						<option value="">Selecione</option>
-					</select>
-					<button class="delete-btn">Cancelar</button>
+	<div id="burger-table">
+		<div>
+			<div id="burger-table-heading">
+				<div class="order-id">#</div>
+				<div>Cliente:</div>
+				<div>Pão:</div>
+				<div>Carne:</div>
+				<div>Opcionais:</div>
+				<div>Ações:</div>
+			</div>
+			<div id="burger-table-rows">
+				<div v-for="burger in burgers" :key="burger.id" class="burger-table-row">
+					<div class="order-number">{{ burger.id }}</div>
+					<div>{{burger.nome}}</div>
+					<div>{{burger.pao}}</div>
+					<div>{{burger.carne}}</div>
+					<div>
+						<ul>
+							<li v-for="(opcional, index) in burger.opcionais" :key="index">
+								{{ opcional }}
+							</li>
+						</ul>
+					</div>
+					<div>
+						<select name="status" class="status">
+							<option v-for="current in status" :key="current.id" value="current.tipo" :selected="burger.status == current.tipo">
+								{{ current.tipo }}	
+							</option>
+						</select>
+						<button class="delete-btn">Cancelar</button>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-  </div>
 </template>
 
 <script>
 export default {
-	name:"Dashboard"
+	name: "Dashboard",
+	data() {
+		return {
+			burgers: null,
+			burger_id: null,
+			status: []
+		}
+	},
+	methods: {
+		async getPedidos(){
+			const req = await fetch('http://localhost:3000/burgers')
+
+			const data = await req.json()
+
+			this.burgers = data
+
+			this.getStatus()
+		},
+		async getStatus(){
+			const req = await fetch('http://localhost:3000/status')
+
+			const data = await req.json()
+
+			this.status = data
+		}
+	},
+	mounted(){
+		this.getPedidos()
+	}
 }
 </script>
 
 <style scoped>
 #burger-table {
-    max-width: 1200px;
-    margin: 0 auto;
-  }
+	max-width: 1200px;
+	margin: 0 auto;
+}
 
-  #burger-table-heading,
-  #burger-table-rows,
-  .burger-table-row {
-    display: flex;
-    flex-wrap: wrap;
-  }
+#burger-table-heading,
+#burger-table-rows,
+.burger-table-row {
+	display: flex;
+	flex-wrap: wrap;
+}
 
-  #burger-table-heading {
-    font-weight: bold;
-    padding: 12px;
-    border-bottom: 3px solid #333;
-  }
+#burger-table-heading {
+	font-weight: bold;
+	padding: 12px;
+	border-bottom: 3px solid #333;
+}
 
-  .burger-table-row {
-    width: 100%;
-    padding: 12px;
-    border-bottom: 1px solid #CCC;
-  }
+.burger-table-row {
+	width: 100%;
+	padding: 12px;
+	border-bottom: 1px solid #CCC;
+}
 
-  #burger-table-heading div,
-  .burger-table-row div {
-    width: 19%;
-  }
+#burger-table-heading div,
+.burger-table-row div {
+	width: 19%;
+}
 
-  #burger-table-heading .order-id,
-  .burger-table-row .order-number {
-    width: 5%;
-  }
+#burger-table-heading .order-id,
+.burger-table-row .order-number {
+	width: 5%;
+}
 
-  select {
-    padding: 12px 6px;
-    margin-right: 12px;
-  }
+select {
+	padding: 12px 6px;
+	margin-right: 12px;
+}
 
-  .delete-btn {
-    background-color: #222;
-    color:#fcba03;
-    font-weight: bold;
-    border: 2px solid #222;
-    padding: 10px;
-    font-size: 16px;
-    margin: 0 auto;
-    cursor: pointer;
-    transition: .5s;
-  }
-  
-  .delete-btn:hover {
-    background-color: transparent;
-    color: #222;
-  }
+.delete-btn {
+	background-color: #222;
+	color: #fcba03;
+	font-weight: bold;
+	border: 2px solid #222;
+	padding: 10px;
+	font-size: 16px;
+	margin: 0 auto;
+	cursor: pointer;
+	transition: .5s;
+}
+
+.delete-btn:hover {
+	background-color: transparent;
+	color: #222;
+}
 </style>
